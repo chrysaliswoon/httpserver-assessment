84da4380055069b73d145f24bf63cc13a36b6726
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-06-23 07:49:56 +0800

    Attempting Task 5

--- src/main/java/assessment/core/HttpClientConnection.java
+++ src/main/java/assessment/core/HttpClientConnection.java
@@ -1,10 +1,18 @@
 package assessment.core;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * this class handles the the request and
 response communication between the server and a client (browser)
  */
 
 public class HttpClientConnection {
-    
+
+    public void start() {
+        // ThreadPoolExecutor threadPool = Executors.newFixedThreadPool(3);
+        // HttpServer server = new HttpServer();
+        // threadPool.execute(server);
+    }
 }
--- src/main/java/assessment/core/HttpServer.java
+++ src/main/java/assessment/core/HttpServer.java
@@ -1,6 +1,9 @@
 package assessment.core;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /*
 this is the main web server class
@@ -14,11 +17,15 @@ public class HttpServer {
 
     public void startServer(Integer PORT) {
         try {
-            // ? Set up the server socket and listen to a port number
             ServerSocket server = new ServerSocket(PORT);
             System.out.println("Web Server is starting up, listening at port " + PORT + ".");
             System.out.println("You can access http://localhost:" + PORT + " now.");
 
+            while (true) {
+                Socket socket = server.accept(); // Once a ServerSocket instance is created,
+                System.out.printf("Connected to PORT: %d \n", PORT);
+
+            }
         } catch (Exception e) {
             System.out.println(e);
             System.exit(1);
